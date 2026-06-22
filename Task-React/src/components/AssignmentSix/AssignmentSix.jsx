0e2ae48d6c964576faf3b48ec2995a1f9a90fe25
prashanthrm74 <prashanthrm74@gmail.com>
2026-06-22 21:01:03 +0530

    updated

--- Task-React/src/components/AssignmentSix/AssignmentSix.jsx
+++ Task-React/src/components/AssignmentSix/AssignmentSix.jsx
@@ -8,40 +8,13 @@ const AssignmentSix = () => {
     const handleTask=()=>{
         navigate("/object-state")
     }
-    const handleTask1=()=>{
-        navigate("/studentnameupdate")
-    }
-    const handleTask2=()=>{
-        navigate("/age")
-    }
-       const handleTask3=()=>{
-        navigate("/theme")
-    }
-
-
-
-
-
+ 
   return (
     <>
 
         <button className="bg-blue-600 text-white px-6 py-2 rounded-xl m-5"
         onClick={handleTask}
         >Task1
-        </button>
-
-        <button className="bg-blue-600 text-white px-6 py-2 rounded-xl m-5"
-        onClick={handleTask1}
-        >Task2
-        </button>
-
-        <button className="bg-blue-600 text-white px-6 py-2 rounded-xl m-5"
-        onClick={handleTask2}
-        >Task3
-        </button>
-         <button className="bg-blue-600 text-white px-6 py-2 rounded-xl m-5"
-        onClick={handleTask3}
-        >Task5
         </button>
     </>
    
