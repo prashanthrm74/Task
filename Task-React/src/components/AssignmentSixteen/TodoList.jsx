cc9354a6fc8f67f8b91cb5233f7ebeea49aa17df
prashanthrm74 <prashanthrm74@gmail.com>
2026-07-10 13:33:19 +0530

    updated

--- Task-React/src/components/AssignmentSixteen/TodoList.jsx
+++ Task-React/src/components/AssignmentSixteen/TodoList.jsx
@@ -1,13 +1,61 @@
+import { useState } from "react";
 
 const TodoList = () => {
 
-    
+ 
+  const [datas, setDatas] = useState("");
+  const [saveData, setSaveData] = useState([]);
+  const [editIndex, setEditIndex] = useState(null);
+
+  const handleClick = () => {
+   
+      // Add new item
+      setSaveData((prev) => [...prev, datas]);
+
+
+    setDatas("");
+  };
 
 
   return (
     <>
+      <div>
+        <h1>Skills Section</h1>
+
+        <input
+          type="text"
+          placeholder="Enter Skills"
+          value={datas}
+          onChange={(e) => setDatas(e.target.value)}
+        />
+
+        <button onClick={handleClick}>
+          {editIndex !== null ? "Update" : "Add"}
+        </button>
+      </div>
+
+      <div>
+        <ol>
+          {saveData.map((skill, index) => (
+            <li key={index}>
+              {skill}
+
+              <button >
+                Edit
+              </button>
+
+              <button>
+                Delete
+              </button>
+            </li>
+          ))}
+        </ol>
+      </div>
     </>
-  )
-}
+  );
+};
+
+
+
 
 export default TodoList
